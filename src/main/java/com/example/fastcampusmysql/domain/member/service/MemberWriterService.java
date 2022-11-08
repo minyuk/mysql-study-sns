186f1bfe0733e02d6aeaca1f5eab7327236f1a4e
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-11-08 17:11:02 +0900

    chore : modify return type in member create

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriterService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriterService.java
@@ -1,5 +1,6 @@
 package com.example.fastcampusmysql.domain.member.service;
 
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
@@ -12,7 +13,7 @@ public class MemberWriterService {
 
     private final MemberRepository memberRepository;
 
-    public Member create(RegisterMemberCommand command) {
+    public MemberDto create(RegisterMemberCommand command) {
         /*
             목표 - 회원정보(이메일, 닉네임, 생년월일)를 등록한다.
                 - 닉네임음 10자를 넘길 수 없다.
@@ -24,6 +25,6 @@ public class MemberWriterService {
                 .birthday(command.birthday())
                 .build();
 
-        return memberRepository.save(member);
+        return memberRepository.save(member).toDto();
     }
 }
